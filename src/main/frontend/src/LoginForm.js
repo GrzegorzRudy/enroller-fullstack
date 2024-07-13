cb125e0abb61b99559fde1238a35d3dd99d6c78a
GrzegorzRudy <111533588+GrzegorzRudy@users.noreply.github.com>
2024-07-13 14:55:41 +0200

    Update LoginForm.js
    update

--- src/main/frontend/src/LoginForm.js
+++ src/main/frontend/src/LoginForm.js
@@ -1,11 +1,18 @@
 import {useState} from "react";
 
-export default function LoginForm({onLogin, buttonLabel}) {
+export default function LoginForm({onAction, buttonLabel}) {
     const [email, setEmail] = useState('');
 
+    const validateEmail = (email) => {
+      return String(email)
+        .toLowerCase()
+        .match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    };
+
     return <div>
-        <label>Zaloguj się e-mailem</label>
+        <label>{buttonLabel || 'Zaloguj się'} e-mailem</label>
         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
+        {(email && !validateEmail(email)) ? <div className="error">Podaj prawidłowy email</div> : ''}
+        <button type="button" onClick={() => onAction(email)} disabled={(!email || !email.includes('@')) ? true : false}>{buttonLabel || 'Wchodzę'}</button>
     </div>;
 }
