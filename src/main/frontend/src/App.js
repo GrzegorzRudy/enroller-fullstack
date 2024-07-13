581521a5e555855b6e2b4dd8aa18bcf0a7857c44
GrzegorzRudy <111533588+GrzegorzRudy@users.noreply.github.com>
2024-07-13 14:04:32 +0200

    Update App.js
    test function register

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,21 +6,72 @@ import UserPanel from "./UserPanel";
 
 function App() {
     const [loggedIn, setLoggedIn] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-    function login(email) {
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser && typeof storedUser === 'string') {
+            setLoggedIn(storedUser);
+        }
+    }, []);
+
+    async function handleLogin(email) {
         if (email) {
-            setLoggedIn(email);
+            setIsLoading(true);
+             const response = await fetch(`/api/participants/${email}`, {
+                 method: 'GET',
+                 headers: { 'Content-Type': 'application/json' }
+             });
+
+            if (response.ok) {
+                localStorage.setItem('user', email)
+                setLoggedIn(email);
+                setIsLoading(false);
+            } else {
+                setIsLoading(false);
+                alert('Nie znaleziono użytkownika o podanym e-mailu. Zarejestruj się.')
+            }
+        }
+    }
+
+    async function handleRegister(email) {
+        if (email) {
+            setIsLoading(true);
+             const response = await fetch(`/api/participants/${email}`, {
+                 method: 'GET',
+                 headers: { 'Content-Type': 'application/json' }
+             });
+
+            if (response.ok) {
+                setIsLoading(false);
+                alert('Podany użytkownik już istnieje. Zaloguj się.')
+            } else {
+                setIsLoading(true);
+                 const response = await fetch('/api/participants', {
+                     method: 'POST',
+                     body: JSON.stringify({'login':email, 'password': '12345'}),
+                     headers: { 'Content-Type': 'application/json' }
+                 });
+
+                 if (response.ok) {
+                     setLoggedIn(email);
+                     localStorage.setItem('user', email);
+                     setIsLoading(false);
+                 }
+            }
         }
     }
 
     function logout() {
         setLoggedIn('');
+        localStorage.setItem('user', '')
     }
 
     return (
-        <div>
-            <h1>System do zapisów na zajęcia:</h1>
-            {loggedIn ? <UserPanel username={loggedIn} onLogout={logout}/> : <LoginForm onLogin={login}/>}
+        <div className="app-wrapper">
+            <h1>System do zapisów na zajęcia</h1>
+            {loggedIn ? <UserPanel username={loggedIn} onLogout={logout}/> : <><LoginForm onAction={handleLogin}/><LoginForm onAction={handleRegister} buttonLabel={"Zarejestruj się"}/></>}
+            {isLoading && <div className="loader lds-dual-ring"></div>}
         </div>
     );
 }
